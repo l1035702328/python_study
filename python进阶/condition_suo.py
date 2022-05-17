a5ad5261d898d93c104a50d6cd82fbb72ac87727
v_jjinnlli <1156989490@qq.com>
2022-05-17 15:17:52 +0800

    测试

--- python进阶/condition_suo.py
+++ python进阶/condition_suo.py
@@ -7,19 +7,22 @@ class Test:
         self._total_value = total_value
         self.__condition = condition
         self._value = 0
+        self._flag = False
 
     def produce(self):
         print("生产者")
         print("每次取出十个水果供应消费者")
-        while self._total_value:
+        while 1:
             with self.__condition:
+                print("等待消费者通知")
+                self.__condition.wait()
                 if self._total_value == 0:
-                    print("没有库存水果了 通知消费者结束这次消费")
-                    self.__condition.notify()
+                    print("没有库存水果了 通知所有消费者结束这次消费")
+                    self._flag = True
+                    self.__condition.notify(2)
                     break
                 else:
                     # 等待消费者通知我送水果
-                    self.__condition.wait()
                     self._value = 10
                     self._total_value -= self._value
                     print("水果库存{}".format(self._total_value))
@@ -29,16 +32,20 @@ class Test:
     def consumer(self):
         print("消费者")
         print("每次取出十个水果消费,需要等待商家送来水果")
-        while self._total_value:
+        while 1:
             with self.__condition:
-                if self._value == 0:
-                    print("没有水果")
+                # 商家水果已无库存 跳出循环
+                if self._flag:
+                    print("{}消费者接收到商家水果已无库存".format(threading.current_thread().getName()))
+                    break
+                if self._value:
+                    self._value -= 1
+                    print("{}正在消费水果:{}".format(threading.current_thread().getName(), self._value))
+                else:
+                    print("{}没有水果".format(threading.current_thread().getName()))
                     # 通知商家送水果顾客处于等待状态
                     self.__condition.notify()
                     self.__condition.wait()
-                else:
-                    self._value -= 1
-                    print("正在消费水果:{}".format(self._value))
 
 
 if __name__ == '__main__':
@@ -46,5 +53,7 @@ if __name__ == '__main__':
     test = Test(1000, condition)
     t1 = threading.Thread(target=test.produce, args=(), daemon=False)
     t2 = threading.Thread(target=test.consumer, args=(), daemon=False)
+    t3 = threading.Thread(target=test.consumer, args=(), daemon=False)
     t1.start()
     t2.start()
+    t3.start()
