7565a9c9d240302d13a5c38b3a3e8836d3ef20b5
v_jjinnlli <1156989490@qq.com>
2022-06-07 16:22:39 +0800

    测试

--- hello1.py
+++ hello1.py
@@ -1,14 +1,6 @@
-class A:
-    def __init__(self):
-        print("a")
+import copy
 
-    def __iter__(self):
-        ss = list(range(20, 1, -2))
-        print(ss)
-        return iter(ss)
-
-
-if __name__ == '__main__':
-    b = A()
-    for i in b:
-        print(i)
+aa = {"bbb":222}
+ss = 222
+for i in aa:
+    print(i)
---
+++ pyceshi/d.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/6/7 16:22
+# @Author  : LZZ
+# @FileName: d.py
+# @Software: PyCharm
